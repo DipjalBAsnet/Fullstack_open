10dcc98d497abcbcb6516ffb27a89cb1e4492d86
DipjalBAsnet <your@gmail.com>
2026-05-02 03:19:57 -0700

    part1 exercise 1.1

---
+++ Part_1/src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = ({course}) => {
+   return (
+    <div>
+        <p>{course}</p>
+    </div>
+   )
+}
+
+export default Header
---
+++ Part_1/src/components/Total.jsx
@@ -0,0 +1,9 @@
+const Total = ({total}) => {
+  return (
+    <div>
+         Total number of exercises is {total}
+    </div>
+  )
+}
+
+export default Total
