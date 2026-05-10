83e0dc7415d05852fc31915f3fe7b0d8f941c111
DipjalBAsnet <your@gmail.com>
2026-05-09 23:03:13 -0700

    part 1 completed

--- Part_1/src/App.jsx
+++ Part_1/src/App.jsx
@@ -1,23 +1,39 @@
-import { useState } from 'react'
-import Display from './components/Display'
-import Button from './components/Button'
+import { useState } from "react"
+import History from "./components/History"
 
 const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
 
-  const [ counter, setCounter ] = useState(0)
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+     console.log('left before', left)
+     const updatedLeft = left + 1
+    setLeft(updatedLeft)
+     console.log('left after', left)
+    setTotal(updatedLeft + right)
+  }
 
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const zero = () => setCounter(0)
+  const handleRightClick = () => {
+  setAll(allClicks.concat('R'))
+  const updatedRight = right + 1
+  setRight(updatedRight)
+  setTotal(left + updatedRight)
+  }
 
-   return (
-    <div>
-      <Display counter={counter}/>
-      <Button onClick={increaseByOne} text ='+'/>
-      <Button onClick={decreaseByOne} text="-" />
-      <Button onClick={zero}text='zero' />
-    </div>
-   )
+  return (
+   <div>
+    {left}
+    <button onClick={handleLeftClick} text="left">left</button>
+    <button onClick={handleRightClick} text="right">right</button>
+    {right}
+    <p>{allClicks.join('')}</p>
+    <p>total {total}</p>
+    <History allClicks={allClicks}/>
+   </div>
+  )
 }
 
 export default App
