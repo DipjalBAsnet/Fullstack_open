b314a473c5c84f2a06cfda6499aeb6783146973f
DipjalBAsnet <your@gmail.com>
2026-06-26 23:39:50 -0700

    ex 2.16

--- parts/Part_2/phonebook/src/App.jsx
+++ parts/Part_2/phonebook/src/App.jsx
@@ -3,12 +3,14 @@ import Filter from "./components/Filter";
 import PersonsForm from "./components/PersonsForm";
 import Persons from "./components/Persons";
 import noteService from "./services/persons";
+import Notification from "./components/Notification";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newPerson, setNewperson] = useState("");
   const [search, setSearch] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [notification, setNotification] = useState(null);
 
   useEffect(() => {
     noteService.getAll().then((persons) => {
@@ -35,6 +37,11 @@ const App = () => {
       setPersons(persons.concat(newPerson));
       setNewperson("");
       setNewNumber("");
+
+      setNotification(`${newPerson.name} added to phonebook`);
+      setTimeout(() => {
+        setNotification(null);
+      }, 5000);
     });
   };
 
@@ -60,12 +67,18 @@ const App = () => {
     if (window.confirm(`Delete ${name}`)) {
       noteService.deletePerson(id).then(() => {
         setPersons(persons.filter((p) => p.id != id));
+
+        setNotification(`${name} deleted from phonebook`);
+        setTimeout(() => {
+          setNotification(null);
+        }, 5000);
       });
     }
   };
   return (
     <div>
       <h1>phonebook</h1>
+      <Notification message={notification} />
       <Filter search={search} handleSearchChange={handleSearchChange} />
       <h2>Add a new</h2>
       <PersonsForm
